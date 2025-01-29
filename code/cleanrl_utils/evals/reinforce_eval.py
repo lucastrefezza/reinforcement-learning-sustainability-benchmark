c20285f8a91ca2a59a8700bc4c130b10d2e4635c
Luca Strefezza <strluca94@gmail.com>
2025-01-29 17:17:14 +0100

    Reinforce evaluation script completed.

---
+++ code/cleanrl_utils/evals/reinforce_eval.py
@@ -0,0 +1,36 @@
+from typing import Callable
+
+import gymnasium as gym
+import torch
+
+
+def evaluate(
+    model_path: str,
+    make_env: Callable,
+    env_id: str,
+    eval_episodes: int,
+    run_name: str,
+    Model: torch.nn.Module,
+    device: torch.device = torch.device("cpu"),
+    capture_video: bool = True,
+):
+    envs = gym.vector.SyncVectorEnv([make_env(env_id, 0, capture_video, run_name)])
+    model = Model(envs).to(device)
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.eval()
+
+    obs, _ = envs.reset()
+    episodic_returns = []
+    while len(episodic_returns) < eval_episodes:
+        actions_distribution = model(torch.Tensor(obs).to(device))
+        action = actions_distribution.sample()
+        next_obs, _, _, _, infos = envs.step(action.cpu().numpy())
+        if "final_info" in infos:
+            for info in infos["final_info"]:
+                if "episode" not in info:
+                    continue
+                print(f"eval_episode={len(episodic_returns)}, episodic_return={info['episode']['r']}")
+                episodic_returns += [info["episode"]["r"]]
+        obs = next_obs
+
+    return episodic_returns
